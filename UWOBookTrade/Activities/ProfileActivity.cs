6ee10ba18132aff497bfcd5d9a3557b526c941f2
calewis1995 <calewis1995@gmail.com>
2018-04-25 19:37:38 -0500

    Can add profile picture from existing pictures on device.

--- UWOBookTrade/Activities/ProfileActivity.cs
+++ UWOBookTrade/Activities/ProfileActivity.cs
@@ -6,7 +6,10 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Database;
+using Android.Graphics;
 using Android.OS;
+using Android.Provider;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
@@ -23,7 +26,7 @@ namespace UWOBookTrade.Activities {
         EditText description;
         ImageView photo;
         byte[] image;
-        string filePath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments), "BookTrade.db3");
+        string filePath = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments), "BookTrade.db3");
 
         protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
@@ -58,8 +61,26 @@ namespace UWOBookTrade.Activities {
 
             if (resultCode == Result.Ok) {
                 photo.SetImageURI(data.Data);
-                //image = File.ReadAllBytes(data.Data.ToString());
+                string filepath = GetPathToImage(data.Data);
+                image = File.ReadAllBytes(filepath);
             }
+        }
+
+        private string GetPathToImage(Android.Net.Uri uri) {
+            ICursor cursor = this.ContentResolver.Query(uri, null, null, null, null);
+            cursor.MoveToFirst();
+            string document_id = cursor.GetString(0);
+            document_id = document_id.Split(':')[1];
+            cursor.Close();
+
+            cursor = ContentResolver.Query(
+            Android.Provider.MediaStore.Images.Media.ExternalContentUri,
+            null, MediaStore.Images.Media.InterfaceConsts.Id + " = ? ", new String[] { document_id }, null);
+            cursor.MoveToFirst();
+            string path = cursor.GetString(cursor.GetColumnIndex(MediaStore.Images.Media.InterfaceConsts.Data));
+            cursor.Close();
+
+            return path;
         }
 
         private void FillNameDescription() {
@@ -72,6 +93,11 @@ namespace UWOBookTrade.Activities {
             }
             if (user.Description != null) {
                 description.Text = user.Description;
+            }
+            if (user.Picture != null) {
+                image = user.Picture;
+                Bitmap bitmap = BitmapFactory.DecodeByteArray(image, 0, image.Length);
+                photo.SetImageBitmap(bitmap);
             }
         }
 
